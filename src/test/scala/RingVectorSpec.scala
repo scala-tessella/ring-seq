0232478680467dc1b1ca9d3a91e5bb51b7e029ac
mcallisto <mario.callisto@gmail.com>
2022-01-02 22:18:22 +0100

    Update RingVectorSpec.scala

--- src/test/scala/RingVectorSpec.scala
+++ src/test/scala/RingVectorSpec.scala
@@ -6,8 +6,12 @@ class RingVectorSpec extends AnyFlatSpec with RingVector with should.Matchers {
 
   val v = Vector(1, 2, 3, 4, 5)
 
-  "A RingVector" can "be rotated" in {
+  "A RingVector" can "be rotated one step to the right" in {
     v.rotateRight(1) shouldBe Vector(5, 1, 2, 3, 4)
+  }
+
+  it can "be rotated one step to the left, starting at index 1" in {
+    v.startAt(1) shouldBe Vector(2, 3, 4, 5, 1)
   }
 
 }
