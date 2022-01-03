d1d3d21d2f06d344fd477a0979f1b6cd9a0a3227
mcallisto <mario.callisto@gmail.com>
2022-01-03 10:52:00 +0100

    Update RingVectorSpec.scala

--- src/test/scala/RingVectorSpec.scala
+++ src/test/scala/RingVectorSpec.scala
@@ -2,7 +2,7 @@ import org.scalatest.*
 import org.scalatest.flatspec.*
 import org.scalatest.matchers.*
 
-class RingVectorSpec extends RingVector with AnyFlatSpec with should.Matchers {
+class RingVectorSpec extends AnyFlatSpec with RingVector with should.Matchers {
 
   val v = Vector(1, 2, 3, 4, 5)
 
