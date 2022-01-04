437401945cbf55b34e1a62ff64dade819751e0d7
mcallisto <mario.callisto@gmail.com>
2022-01-04 18:32:52 +0100

    Update SymmetriesSpec.scala

--- src/test/scala/SymmetriesSpec.scala
+++ src/test/scala/SymmetriesSpec.scala
@@ -9,20 +9,24 @@ class SymmetriesSpec extends AnyFlatSpec with RingVector with should.Matchers {
 
   val spin3 = Vector(1, 2, 3, 1, 2, 3, 1, 2, 3)
   val eptagon = Vector(6, 6, 6, 6, 6, 6, 6)
+  val squaroid = Vector(2, 1, 2, 2, 1, 2, 2, 1, 2, 2, 1, 2)
 
   "A Vector considered as a ring" can "have n-fold rotational symmetry" in {
     spin3.rotationalSymmetry shouldBe 3
     eptagon.rotationalSymmetry shouldBe 7
+    squaroid.rotationalSymmetry shouldBe 4
   }
 
   it can "have axes of reflectional symmetry" in {
     spin3.symmetry shouldBe 0
-    eptagon.symmetry shouldBe 7 
+    eptagon.symmetry shouldBe 7
+    squaroid.symmetry shouldBe 4
   }
 
   it can "return the indices closer to the axes of reflectional symmetry" in {
     spin3.symmetryIndices shouldBe Nil
-    eptagon.symmetry shouldBe List(0, 1, 2, 3, 4, 5, 6) 
+    eptagon.symmetryIndices shouldBe List(0, 1, 2, 3, 4, 5, 6) 
+    squaroid.symmetryIndices shouldBe List(1, 4, 7, 10) 
   }
 
 }
