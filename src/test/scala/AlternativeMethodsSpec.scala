efee83dab97d86e85eb3ff4d936103c4d2a51847
mcallisto <mario.callisto@gmail.com>
2022-01-04 12:05:40 +0100

    Update AlternativeMethodsSpec.scala

--- src/test/scala/AlternativeMethodsSpec.scala
+++ src/test/scala/AlternativeMethodsSpec.scala
@@ -10,29 +10,35 @@ class AlternativeMethodsSpec extends AnyFlatSpec with RingVector with should.Mat
   val v = Vector(1, 2, 3, 4, 5)
 
   "A Vector considered as a ring" must "always have an element indexed before another one" in {
-    assertThrows[IndexOutOfBoundsException] { // Result type: Assertion
+    assertThrows[IndexOutOfBoundsException] {
       v.apply(-1)
     }
     v.applyO(-1) shouldBe 5
   }
 
   it must "always have an element indexed after another one" in {
-    assertThrows[IndexOutOfBoundsException] { // Result type: Assertion
+    assertThrows[IndexOutOfBoundsException] {
       v.apply(5)
     }
     v.applyO(5) shouldBe 1
   }
 
   "An empty Vector" must "have no indexed elements" in {
-    assertThrows[IndexOutOfBoundsException] { // Result type: Assertion
+    assertThrows[ArithmeticException] {
       Vector.empty.apply(0)
     }
-    assertThrows[IndexOutOfBoundsException] { // Result type: Assertion
+    assertThrows[ArithmeticException] {
       Vector.empty.applyO(0)
     }
   }
 
-  it must "return an element for any index" in {
+  "Any non empty Vector" must "return an element for any index " in {
+    val gen: Gen[(Vector[Int], Int)] =
+      for
+        list <- Gen.nonEmptyContainerOf[List,Int](Gen.oneOf(1, 3, 5))
+        i <- Gen.choose(-1000, 1000)
+      yield (list.toVector, i)
+
     check(forAll { (i: Int) => v.contains(v.applyO(i)) })
   }
 
