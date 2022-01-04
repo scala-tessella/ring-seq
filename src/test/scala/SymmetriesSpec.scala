26718c8f38e8b5713ef42b55b29423067ee7b677
mcallisto <mario.callisto@gmail.com>
2022-01-04 18:11:31 +0100

    Update SymmetriesSpec.scala

--- src/test/scala/SymmetriesSpec.scala
+++ src/test/scala/SymmetriesSpec.scala
@@ -8,9 +8,16 @@ import org.scalatest.matchers.*
 class SymmetriesSpec extends AnyFlatSpec with RingVector with should.Matchers {
 
   val v = Vector(1, 2, 3, 1, 2, 3, 1, 2, 3)
+  val eptagon = Vector(6, 6, 6, 6, 6, 6, 6)
 
   "A Vector considered as a ring" can "have n-fold rotational symmetry" in {
     v.rotationalSymmetry shouldBe 3
+    eptagon.rotationalSymmetry shouldBe 3
+  }
+
+  it can "have axes of reflectional symmetry" in {
+    v.symmetry shouldBe 0
+    eptagon.symmetry shouldBe 7 
   }
 
 }
