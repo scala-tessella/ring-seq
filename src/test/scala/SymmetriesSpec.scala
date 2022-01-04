63fbaf6f8f728ec63da471e8c30e70c82d30d5e8
mcallisto <mario.callisto@gmail.com>
2022-01-04 14:26:07 +0100

    Update and rename RingVectorSpec.scala to SymmetriesSpec.scala

--- src/test/scala/SymmetriesSpec.scala
+++ src/test/scala/SymmetriesSpec.scala
@@ -5,20 +5,24 @@ import org.scalatest.*
 import org.scalatest.flatspec.*
 import org.scalatest.matchers.*
 
-class RingVectorSpec extends AnyFlatSpec with RingVector with should.Matchers {
+class SymmetriesSpec extends AnyFlatSpec with RingVector with should.Matchers {
 
   val v = Vector(1, 2, 3, 4, 5)
 
-  "A RingVector" can "be rotated one step to the right" in {
+  "A Vector considered as a ring" can "be rotated one step to the right" in {
     v.rotateRight(1) shouldBe Vector(5, 1, 2, 3, 4)
   }
 
-  it can "be rotated one step to the left, now starts where index 1 was" in {
+  it can "be rotated one step to the left, to start where index 1 was" in {
     v.startAt(1) shouldBe Vector(2, 3, 4, 5, 1)
   }
 
   it can "be reflected" in {
     v.reflectAt() shouldBe Vector(1, 5, 4, 3, 2)
+  }
+
+  it can "be reflected at a given index" in {
+    v.reflectAt(2) shouldBe Vector(3, 2, 1, 5, 4)
   }
 
 }
