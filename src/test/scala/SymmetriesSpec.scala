17e05612729894e3b2f8687a4dd70200f556c789
mcallisto <mario.callisto@gmail.com>
2022-01-05 21:41:58 +0100

    Update SymmetriesSpec.scala

--- src/test/scala/SymmetriesSpec.scala
+++ src/test/scala/SymmetriesSpec.scala
@@ -8,6 +8,7 @@ import org.scalatest.matchers.*
 class SymmetriesSpec extends AnyFlatSpec with RingVector with should.Matchers {
 
   val v = Vector(1, 2, 3, 4, 5)
+  val empty = Vector.empty
   val spin3 = Vector(1, 2, 3, 1, 2, 3, 1, 2, 3)
   val eptagon = Vector(6, 6, 6, 6, 6, 6, 6)
   val squaroid = Vector(2, 1, 2, 2, 1, 2, 2, 1, 2, 2, 1, 2)
@@ -17,6 +18,7 @@ class SymmetriesSpec extends AnyFlatSpec with RingVector with should.Matchers {
 
   "A Vector considered as a ring" can "have n-fold rotational symmetry" in {
     v.rotationalSymmetry shouldBe 1
+    empty.rotationalSymmetry shouldBe 1
     spin3.rotationalSymmetry shouldBe 3
     eptagon.rotationalSymmetry shouldBe 7
     squaroid.rotationalSymmetry shouldBe 4
@@ -27,6 +29,7 @@ class SymmetriesSpec extends AnyFlatSpec with RingVector with should.Matchers {
 
   it can "have axes of reflectional symmetry" in {
     v.symmetry shouldBe 0
+    empty.symmetry shouldBe 0
     spin3.symmetry shouldBe 0
     eptagon.symmetry shouldBe 7
     squaroid.symmetry shouldBe 4
@@ -37,6 +40,7 @@ class SymmetriesSpec extends AnyFlatSpec with RingVector with should.Matchers {
 
   it can "return the indices closer to the axes of reflectional symmetry" in {
     v.symmetryIndices shouldBe Nil
+    empty.symmetryIndices shouldBe Nil
     spin3.symmetryIndices shouldBe Nil
     eptagon.symmetryIndices shouldBe List(0, 1, 2, 3, 4, 5, 6) 
     squaroid.symmetryIndices shouldBe List(1, 4, 7, 10)
@@ -45,10 +49,10 @@ class SymmetriesSpec extends AnyFlatSpec with RingVector with should.Matchers {
     axisOnOffElement.symmetryIndices shouldBe List(0)
   }
 
-  "Any non empty Vector" must "have rotational higher or equal than reflectional symmetry" in {
+  "Any Vector" must "have rotational higher or equal than reflectional symmetry" in {
     val gen: Gen[Vector[Int]] =
       for
-        list <- Gen.nonEmptyContainerOf[List, Int](Gen.oneOf(1, 3, 5))
+        list <- Gen.containerOf[List, Int](Gen.oneOf(1, 3, 5))
       yield list.toVector
     check(
       forAll(gen)(vector => vector.rotationalSymmetry >= vector.symmetry)
