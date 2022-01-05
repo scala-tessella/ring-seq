5516a06681717cb2c12af5931bd9c2f0d8c34222
mcallisto <mario.callisto@gmail.com>
2022-01-05 19:12:30 +0100

    Update SymmetriesSpec.scala

--- src/test/scala/SymmetriesSpec.scala
+++ src/test/scala/SymmetriesSpec.scala
@@ -1,3 +1,6 @@
+import org.scalacheck.Gen
+import org.scalacheck.Prop.forAll
+import org.scalacheck.Test.check
 import org.scalatest.*
 import org.scalatest.flatspec.*
 import org.scalatest.matchers.*
