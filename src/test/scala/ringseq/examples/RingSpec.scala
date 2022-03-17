014a0db97a8eac7023bb3341c5372a7052cb3fde
Mario Càllisto <mario.callisto@gmail.com>
2022-03-18 00:36:07 +0100

    Improved description

--- src/test/scala/ringseq/examples/RingSpec.scala
+++ src/test/scala/ringseq/examples/RingSpec.scala
@@ -8,14 +8,14 @@ class RingSpec extends AnyFlatSpec with should.Matchers {
   val underlying = List(1, 2, 3, 4)
   val ring = new Ring(underlying)
 
-  "An instance of a examples.Ring class" must "be created with no rotation and no reversion" in {
+  "An instance of an examples.Ring class" must "be created with no rotation and no reversion" in {
     ring.currentHead shouldBe 1
     ring.headIndex shouldBe 0
     ring.isReversed shouldBe false
     ring.current shouldBe underlying
   }
 
-  it can "be rotated by 1 step to the right" in {
+  it can "be then rotated by 1 step to the right" in {
     ring.rotateR()
     ring.headIndex shouldBe -1
     ring.isReversed shouldBe false
@@ -23,7 +23,7 @@ class RingSpec extends AnyFlatSpec with should.Matchers {
     ring.current shouldBe List(4, 1, 2, 3)
   }
 
-  it can "be rotated back by 1 step to the left" in {
+  it can "be then rotated back by 1 step to the left" in {
     ring.rotateL()
     ring.headIndex shouldBe 0
     ring.isReversed shouldBe false
@@ -31,7 +31,7 @@ class RingSpec extends AnyFlatSpec with should.Matchers {
     ring.current shouldBe underlying
   }
 
-  it can "be reversed" in {
+  it can "be then reversed" in {
     ring.reverse()
     ring.headIndex shouldBe 0
     ring.isReversed shouldBe true
@@ -39,7 +39,7 @@ class RingSpec extends AnyFlatSpec with should.Matchers {
     ring.current shouldBe List(1, 4, 3, 2)
   }
 
-  it can "be rotated while reversed by 1 step to the right" in {
+  it can "be then rotated while reversed by 1 step to the right" in {
     ring.rotateR(1)
     ring.headIndex shouldBe 1
     ring.isReversed shouldBe true
@@ -47,7 +47,7 @@ class RingSpec extends AnyFlatSpec with should.Matchers {
     ring.current shouldBe List(2, 1, 4, 3)
   }
 
-  it can "be reversed again" in {
+  it can "be then reversed again" in {
     ring.reverse()
     ring.headIndex shouldBe 1
     ring.isReversed shouldBe false
@@ -55,7 +55,7 @@ class RingSpec extends AnyFlatSpec with should.Matchers {
     ring.current shouldBe List(2, 3, 4, 1)
   }
 
-  it can "be rotated back by 1 step to the right" in {
+  it can "be then rotated back by 1 step to the right" in {
     ring.rotateR(1)
     ring.headIndex shouldBe 0
     ring.isReversed shouldBe false
