5dccf7c15cc221f13a815c506b940eae7c885cf6
Mario Càllisto <mario.callisto@gmail.com>
2022-04-02 01:55:01 +0200

    Unused import

--- src/main/scala-3/io/github/scala_tessella/ring_seq/RingSeq.scala
+++ src/main/scala-3/io/github/scala_tessella/ring_seq/RingSeq.scala
@@ -1,7 +1,5 @@
 package io.github.scala_tessella.ring_seq
 
-import scala.collection.SeqOps
-
 /** Adds implicit methods to `[[https://www.scala-lang.org/api/current/scala/collection/Seq.html Seq]]`
  * (immutable / mutable and subtypes) for when a sequence needs to be considered '''circular''', its elements forming a ring.
  *
