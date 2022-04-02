c05e987dfec3a7c59699d23419901984eec70a77
Mario Càllisto <mario.callisto@gmail.com>
2022-04-02 12:09:27 +0200

    Revert "traits in objects"
    This reverts commit 7360b29991fdaa373de1a76347f60fb9b1cb3283.

--- src/main/scala-2.13/io/github/scala_tessella/ring_seq/IndexingOps.scala
+++ src/main/scala-2.13/io/github/scala_tessella/ring_seq/IndexingOps.scala
@@ -2,35 +2,31 @@ package io.github.scala_tessella.ring_seq
 
 import scala.collection.SeqOps
 
-object IndexingOps {
+/** Universal trait providing decorators for a `Seq` considered circular. */
+trait IndexingOps[A, CC[B] <: SeqOps[B, CC, CC[B]]] extends Any {
 
-  /** Universal trait providing decorators for a `Seq` considered circular. */
-  trait IndexingDecorators[A, CC[B] <: SeqOps[B, CC, CC[B]]] extends Any {
+  /** For improved readability, the index of a `Seq`. */
+  type Index = Int
 
-    /** For improved readability, the index of a `Seq`. */
-    type Index = Int
+  /** For improved readability, the index of a circular `Seq`.
+   *
+   * @note any value is a valid index, provided that `Seq` is not empty
+   */
+  type IndexO = Int
 
-    /** For improved readability, the index of a circular `Seq`.
-     *
-     * @note any value is a valid index, provided that `Seq` is not empty
-     */
-    type IndexO = Int
+  /** The circular sequence */
+  def ring: CC[A]
 
-    /** The circular sequence */
-    def ring: CC[A]
+  def indexFrom(i: IndexO): Index =
+    java.lang.Math.floorMod(i, ring.size)
 
-    def indexFrom(i: IndexO): Index =
-      java.lang.Math.floorMod(i, ring.size)
-
-    /** Gets the element at some circular index.
-     *
-     * @param i [[IndexO]]
-     * @throws java.lang.ArithmeticException if `Seq` is empty
-     * @example {{{Seq(0, 1, 2).applyO(3) // 0}}}
-     */
-    def applyO(i: IndexO): A =
-      ring(indexFrom(i))
-
-  }
+  /** Gets the element at some circular index.
+   *
+   * @param i [[IndexO]]
+   * @throws java.lang.ArithmeticException if `Seq` is empty
+   * @example {{{Seq(0, 1, 2).applyO(3) // 0}}}
+   */
+  def applyO(i: IndexO): A =
+    ring(indexFrom(i))
 
 }
